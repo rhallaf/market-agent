f289fc1d46dfc9fd7ba2a270e67fe75ec6ecb406
rhallaf <hallafrashid@gmail.com>
2026-06-12 10:07:01 +0200

    fix CORS

--- backend/main.py
+++ backend/main.py
@@ -11,6 +11,7 @@ app.add_middleware(
     allow_origins=["*"],
     allow_methods=["*"],
     allow_headers=["*"],
+    allow_credentials=True,
 )
 
 app.include_router(markets.router, prefix="/api/markets", tags=["Markets"])
